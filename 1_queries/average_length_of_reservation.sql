2479005e02c3db8038248b6dd04bfd7d6f31aa4a
Shakirat komolafe <damolakomolafe.dk@gmail.com>
2021-08-18 17:32:59 -0400

    add query operation for the database

---
+++ 1_queries/average_length_of_reservation.sql
@@ -0,0 +1,2 @@
+SELECT avg(end_date - start_end) as average_duration
+FROM reservations;
---
+++ 1_queries/property_listings_by_city.sql
@@ -0,0 +1,11 @@
+ SELECT properties.* avg(property_reviews.ratings) as average_rating
+ FROM properties
+ JOIN property_reviews ON property_id = properties.id
+ WHERE city LIKE '%ancouv%'
+ GROUP BY properties.id
+ HAVING avg(property_reviews.rating) >= 4
+ ORDER BY cost_per_night 
+ LIMIT 10;
+
+
+ 
