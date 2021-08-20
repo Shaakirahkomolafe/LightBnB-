e88c20deda409b3ffcc0bdbe94de99d241289935
Shakirat komolafe <damolakomolafe.dk@gmail.com>
2021-08-19 22:01:39 -0400

    fix bugs

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -23,7 +23,7 @@ const getUserWithEmail = (email) => {
   return pool
     .query(`SELECT * FROM users WHERE email = $1`, [email])
     .then((result) => {
-      return (result.rows);
+      return (result.rows[0]);
     })
     .catch((err) => {
       return (err.message);
@@ -41,7 +41,7 @@ const getUserWithId = (id) => {
   return pool
     .query(`SELECT * FROM users WHERE id = $1`, [id])
     .then((result) => {
-      return (result.rows);
+      return (result.rows[0]);
     }).catch((err) => {
       return (err.message);
     });
@@ -76,8 +76,8 @@ exports.addUser = addUser;
  */
 const getAllReservations = (guest_id, limit = 10) => {
   return pool
-    .query = `SELECT reservations.*, properties.* FROM reservations 
-    JOIN properties ON property_id=properties.id WHERE guest_id=$1 LIMIT $2`,[guest_id, limit]
+    .query(`SELECT reservations.*, properties.* FROM reservations 
+    JOIN properties ON property_id=properties.id WHERE guest_id=$1 LIMIT $2`,[guest_id, limit])
     .then((result) => {
       return (result.rows);
     }).catch((err) => {
@@ -105,25 +105,25 @@ const getAllProperties = function(options, limit = 10) {
   SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
   JOIN property_reviews ON properties.id = property_id
-  WHERE 1 = 1`;
+  `;
 
   // 3
   if (options.city) {
     queryParams.push(`%${options.city}%`);
-    queryString += `AND city LIKE $${queryParams.length} `;
+    queryString += `WHERE city LIKE $${queryParams.length} `;
   }
 
   // 4
   if (options.owner_id) {
     queryParams.push(`${options.owner_id}`);
-    queryString += `AND owner_id = $${queryParams.length} `;
+    queryString += `WHERE owner_id = $${queryParams.length} `;
 
   }
   if (options.minimum_price_per_night && options.maximum_price_per_night) {
     queryParams.push(`${options.minimum_price_per_night}`);
     queryParams.push(`${options.maximum_price_per_night}`);
-    queryString += `AND cost_per_night / 100 >= $${queryParams.length - 1}`;
-    queryString += `AND cost_per_night / 100 < $${queryParams.length}`;
+    queryString += `AND cost_per_night >= $${queryParams.length - 1} * 100`;
+    queryString += `AND cost_per_night < $${queryParams.length} * 100`;
   }
   // if a minimum_rating is passed in, only return properties with a rating equal to or higher than that.
   if (options.minimum_rating) {
@@ -131,8 +131,11 @@ const getAllProperties = function(options, limit = 10) {
     queryString += `
     GROUP BY properties.id
     HAVING avg(property_reviews.rating) >= $${queryParams.length}`;
+    queryParams.push(limit);
+    queryString +=
+    `ORDER BY cost_per_night
+    LIMIT $${queryParams.length};`;
   } else {
-
     queryParams.push(limit);
     queryString += `
   GROUP BY properties.id
@@ -153,14 +156,15 @@ exports.getAllProperties = getAllProperties;
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function(property) {
+const addProperty = (property) => {
   return pool
-    .query = `INSERT INTO properties (owner_id,title,
-  description,thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, 
+    .query(`INSERT INTO properties (owner_id,title,
+  description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, 
   province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
-  values ($1,$2, $3, $4, $5, $6, $7, $8,$9, $10, $11, $12, $13, $14), 
-`, [properties.owner_id, properties.title, properties.description,properties.properties.thumbnail_photo_url, properties.cover_photo_url, properties.cost_per_night, properties.street, properties.city, properties.province, properties.post_code, properties.country, properties.parking_spaces, properties.number_of_bathrooms,properties.number_of_bedrooms]
+  values ($1,$2, $3, $4, $5, $6, $7, $8,$9, $10, $11, $12, $13, $14)RETURNING *;
+`, [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms])
     .then((result) => {
+      console.log(result.rows);
       return (result.rows);
     }).catch((err) => {
       return (err.message);
